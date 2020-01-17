cf48d85f29a3b3f5e9d5b7c5f7864b89091657e5
Emeka Egwim <egwim.emeka@gmail.com>
2020-01-17 13:07:48 -0800

    Implemented Recipe Service, Added Nectar stylesheet & Updated Index Controller w/ UI View

--- src/main/java/iot/empiaurhouse/ambrosia/controllers/IndexController.java
+++ src/main/java/iot/empiaurhouse/ambrosia/controllers/IndexController.java
@@ -1,48 +1,22 @@
 package iot.empiaurhouse.ambrosia.controllers;
 
-import iot.empiaurhouse.ambrosia.model.*;
-import iot.empiaurhouse.ambrosia.repositories.CategoryRepository;
-import iot.empiaurhouse.ambrosia.repositories.RecipeRepository;
-import iot.empiaurhouse.ambrosia.repositories.UnitOfMeasureRepository;
+import iot.empiaurhouse.ambrosia.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    private RecipeRepository recipeRepository;
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-
-    public IndexController(RecipeRepository recipeRepository, CategoryRepository categoryRepository,
-                           UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.recipeRepository = recipeRepository;
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"","/","/index"})
-    public String getIndexPage(){
-
-        Optional<Category> categoryOptional = categoryRepository.findByCategoryDescription("Lunch");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByUom("Pound");
-
-        Recipe eguisi = new Recipe();
-        Synopsis synopsis = new Synopsis();
-        Cuisine chinese = new Cuisine();
-        chinese.setCuisineDescription("Chinese");
-        synopsis.setRecipeSynopsis("Eguisi");
-        eguisi.setDescription("Eguisi");
-        eguisi.getCuisines().add(chinese);
-
-
-        System.out.println("Recipe Category ID is: " + categoryOptional.get().getId());
-        System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
-        System.out.println(eguisi.getDescription());
-        System.out.println(eguisi.getCuisines());
+    public String getIndexPage(Model recipeModel){
+        recipeModel.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 
---
+++ src/main/java/iot/empiaurhouse/ambrosia/services/RecipeService.java
@@ -0,0 +1,11 @@
+package iot.empiaurhouse.ambrosia.services;
+
+import iot.empiaurhouse.ambrosia.model.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+
+}
